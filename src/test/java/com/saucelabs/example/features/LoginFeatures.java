440b49143f3af83240efefc22360f9cc7512104b
Bill Meyer <thebillmeyer@gmail.com>
2020-10-21 14:22:46 -0500

    Changes to use new unified platform and W3C standards

--- src/test/java/com/saucelabs/example/features/LoginFeatures.java
+++ src/test/java/com/saucelabs/example/features/LoginFeatures.java
@@ -4,7 +4,7 @@ import com.saucelabs.example.pages.InventoryPage;
 import com.saucelabs.example.pages.LoginPage;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.MutableCapabilities;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -14,23 +14,49 @@ public class LoginFeatures
 {
     private final static String userName = System.getenv("SAUCE_USERNAME");
     private final static String accessKey = System.getenv("SAUCE_ACCESS_KEY");
-//        private final static String platformName = "Android";
-//        private final static String platformVersion = "9";
-//        private final static String deviceName = "Google_Pixel_3_POC150";
-    private final static String platformName = "iOS";
-    private final static String platformVersion = "12.1.4";
-    private final static String deviceName = "iPhone_XS_POC166";
+
+    // Run on a real Android device
+    private final static String platformName = "Android";
+    private final static String platformVersion = "10";
+    private final static String deviceName = "Google.*";
+
+    // Run on a real iOS device
+//    private final static String platformName = "iOS";
+//    private final static String platformVersion = "12.1.4";
+//    private final static String deviceName = "iPhone.*";
+
+    // Run on an Android Emulator
+//    private final static String platformName = "Android";
+//    private final static String platformVersion = "11";
+//    private final static String deviceName = "Android GoogleAPI Emulator";
+
+    // Run on an iOS Simulator
+//    private final static String platformName = "iOS";
+//    private final static String platformVersion = "13.4";
+//    private final static String deviceName = "iPhone XS Simulator";
 
     @Test
     public void verifyValidUsersCanSignIn()
     throws MalformedURLException
     {
-        URL url = new URL("https://" + userName + ":" + accessKey + "@ondemand.us-west-1.saucelabs.com/wd/hub");
+        URL url = new URL("https://ondemand.us-west-1.saucelabs.com/wd/hub");
 
-        DesiredCapabilities caps = new DesiredCapabilities();
+        // Set the W3C standard capabilities...
+        MutableCapabilities caps = new MutableCapabilities();
         caps.setCapability("platformName", platformName);
         caps.setCapability("platformVersion", platformVersion);
+        caps.setCapability("browserName", "chrome");
         caps.setCapability("name", "Verify Valid Users Can Sign In");
+
+        // Set the Sauce-specific capabilities...
+        MutableCapabilities sauceOpts = new MutableCapabilities();
+        sauceOpts.setCapability("userName", userName);
+        sauceOpts.setCapability("accessKey", accessKey);
+        sauceOpts.setCapability("appiumVersion", "1.18.1");
+        sauceOpts.setCapability("deviceName", deviceName);
+
+        // Merge them together.
+        caps.setCapability("sauce:options", sauceOpts);
 
         AppiumDriver driver = new AppiumDriver(url, caps);
 
